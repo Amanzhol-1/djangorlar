36b1f343be5d269d27f5390fd696b3ec4467783e
Lain <amanzhol.moltabarov@gmail.com>
2025-11-17 16:50:52 +0500

    create auths app

--- settings/base.py
+++ settings/base.py
@@ -27,6 +27,7 @@ DJANGO_AND_THIRD_PARTY_APPS = [
 PROJECT_APPS = [
     "apps.tasks.apps.TasksConfig",
     "apps.abstracts.apps.AbstractsConfig",
+    "apps.auths.apps.AuthsConfig",
 ]
 INSTALLED_APPS = DJANGO_AND_THIRD_PARTY_APPS + PROJECT_APPS
 
