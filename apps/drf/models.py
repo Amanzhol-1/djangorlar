36d8009c07e4122a7adb536daf5d63d3ffb55c8a
Lain <amanzhol.moltabarov@gmail.com>
2025-12-01 18:22:22 +0500

    add Course and Lesson models

---
+++ apps/drf/models.py
@@ -0,0 +1,32 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
+from django.contrib.auth import get_user_model
+from apps.abstracts.models import AbstractBaseModel
+
+
+TITLE_MAX_LENGTH = 50
+ORDER_MAX_LEN = 10
+ORDER_DECIMAL_PLACES = 2
+INDENTATION_MAX_VALUE = 50
+
+User = get_user_model()
+
+class Course(AbstractBaseModel):
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
+    description = models.TextField()
+    owner = models.ForeignKey(User, related_name='owned_courses', on_delete=models.CASCADE)
+
+
+class Lesson(AbstractBaseModel):
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
+    content = models.TextField()
+    order = models.DecimalField(max_digits=ORDER_MAX_LEN, decimal_places=ORDER_DECIMAL_PLACES)
+    indentation = models.PositiveSmallIntegerField(
+        default=0,
+        validators=[MaxValueValidator(INDENTATION_MAX_VALUE)]
+    )
+    is_published = models.BooleanField(default=False)
+    course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} ({self.course.title})"
--- settings/base.py
+++ settings/base.py
@@ -29,6 +29,7 @@ PROJECT_APPS = [
     "apps.tasks.apps.TasksConfig",
     "apps.abstracts.apps.AbstractsConfig",
     "apps.auths.apps.AuthsConfig",
+    "apps.drf.apps.DrfConfig",
 ]
 INSTALLED_APPS = DJANGO_AND_THIRD_PARTY_APPS + PROJECT_APPS
 
